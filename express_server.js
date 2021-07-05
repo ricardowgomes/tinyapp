764daacdeb8c5de8555c50ea722c63204575ace1
Ricardo W. Gomes <ricardowgomes@outlook.com>
2021-07-05 21:34:31 +0000

    added bodyParser

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
@@ -9,12 +12,18 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-
+// CREATES A GET ROUTE TO urls_index
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+// CREATES A GET ROUTE TO urls_new
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+// CREATES A GET ROUTE TO urls_show
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
