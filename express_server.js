78ccf4e2f8190c64a3b63f30c48406e460228d66
Ricardo W. Gomes <ricardowgomes@outlook.com>
2021-07-07 03:51:15 +0000

    added login/logout logic

--- express_server.js
+++ express_server.js
@@ -3,7 +3,10 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
+
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.set('view engine', 'ejs');
 
@@ -14,16 +17,18 @@ const urlDatabase = {
 
 // CREATES A GET ROUTE TO urls_index
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
 // CREATES A GET ROUTE TO urls_new
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+
+  res.render("urls_new", templateVars);
 });
 
-// CREATES A POST ROUTE TO urls_new
+// CREATES A POST ROUTE FROM SUBMIT NEW
 app.post("/urls", (req, res) => {
   const randomString = generateRandomString();
   urlDatabase[randomString] = req.body.longURL;
@@ -32,7 +37,7 @@ app.post("/urls", (req, res) => {
 
 // CREATES A GET ROUTE TO urls_show
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
@@ -46,18 +51,28 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+// CREATES A POST ROUTE TO LOGIN
+app.post("/urls/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  const username = req.body.username;
+
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+
+  console.log(username);
+  // console.log(usernameCookieParse);
+  res.render("urls_index", templateVars);
 });
 
+// CREATES A POST ROUTE TO DELETE
 app.post("/urls/:shortURL/delete", (req, res) => {
+  console.log(urlDatabase[req.params.shortURL]);
   delete urlDatabase[req.params.shortURL];
   res.redirect("/urls");
 });
 
+// CREATES A POST ROUTE TO EDIT
 app.post("/urls/:shortURL/edit", (req, res) => {
   const newUrl = req.body.editURL;
-  console.log(newUrl);
   urlDatabase[req.params.shortURL] = newUrl;
   res.redirect("/urls");
 });
